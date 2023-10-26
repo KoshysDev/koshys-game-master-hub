3ab33ff91cf979a0e43030d19f5497f346e0beb5
KoshysDev <koshys.osu@gmail.com>
2023-10-26 18:32:54 +0300

    miniatures creation window

---
+++ GM_hub/Assets/Scripts/UI/UtilsUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UtilsUI : MonoBehaviour
+{
+    public void DestroyUIComponent(GameObject window)
+    {
+        Destroy(window);
+    }
+}
